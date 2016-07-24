95621f0b825c1d8affd31c810372cb4f93d2cb02
guzhenfu <gzfgeh@gmail.com>
2016-07-25 07:34:23 +0800

    add list view

--- www/js/mainCtrl.js
+++ www/js/mainCtrl.js
@@ -1,5 +1,9 @@
 angular.module('starter.controllers')
-
 .controller('mainCtrl', function($scope){
+  $scope.offerList = [];
+  for(var i=0; i<10; i++){
+    $scope.offerList.push(['item list ' + i+1].join(''));
+  }
+
 
 });
