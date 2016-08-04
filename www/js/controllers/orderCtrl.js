a545eb8c4725fe00fa404b0692b58c7fecb1cca6
guzhenfu <gzfgeh@gmail.com>
2016-08-04 21:54:29 +0800

    add refresh list

--- www/js/controllers/orderCtrl.js
+++ www/js/controllers/orderCtrl.js
@@ -1,5 +1,15 @@
 angular.module('starter.controllers')
 
 .controller('orderCtrl', function($scope){
+  $scope.lightClick = function () {
+    console.log("light");
+    window.document.getElementById("order").style.backgroundColor="#000";
+    window.document.getElementById("order").style.color="#FFF";
+  };
 
+  $scope.redClick = function () {
+    console.log("red");
+    window.document.getElementById("order").style.backgroundColor="#CCC";
+    window.document.getElementById("order").style.color="#222";
+  };
 });
